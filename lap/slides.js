71acd64cc04119df597809978a5ab972343cbed1
chamuditha pasindu <chamudithapasindu54@gmail.com>
2025-06-23 13:22:25 +0530

    test doc
    Co-Authored-By: SLRAPTOR <63906862+SLRAPTOR@users.noreply.github.com>

---
+++ lap/slides.js
@@ -0,0 +1,10 @@
+/*var x=1;
+
+function b(){
+    var y=document.getElementById("asus");
+    y.className='dvi-1 asus-'+x;
+    x++;
+    if(x===4){
+        x=1;
+    }
+}
---
+++ php/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+    <body>
+        <div class="bolechtimalli">
+            <img src="../imgs/home.png" class="img">
+            <img src="../imgs/previous.png" class="img">
+            <img src="../imgs/menu.png" class="img" style=" margin-left: 90%; margin-top: -40%">
+        </div>
+        <div>
+            <center>
+                
+                <h1>Welcome to admin panel</h1><br>
+                <img src="../imgs/add-user.png" class="sudu-chuti" > &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+                <img src="../imgs/verify.png" class="sudu-chuti">&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+                <img src="../imgs/shopping-cart.png" class="sudu-chuti">&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+
+
+
+            </center>
+
+        </div>
+
+
+
+    </body>
+</html>
